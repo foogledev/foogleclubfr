22ae4b2eb2230cae461b578fd5e736202b7aac33
foogle fr <32602676+foogledev@users.noreply.github.com>
2022-07-28 16:02:36 -0400

    Update club.lua

--- club.lua
+++ club.lua
@@ -1,1 +1,5 @@
+repeat wait() until game:IsLoaded()
 
+if game.PlaceId == 10266164381 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/foogledev/foogleclubfr/main/bloodlines.lua"))()
+end
