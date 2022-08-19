b2a22b2fe5ea9b0d759b9a206fd6ab3a91173e12
foogle fr <32602676+foogledev@users.noreply.github.com>
2022-08-18 23:58:07 -0400

    Update club.lua

--- club.lua
+++ club.lua
@@ -4,8 +4,11 @@ getgenv().Games = {
     [10266164381] = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/foogledev/foogleclubfr/main/games/bloodlines.lua"))() 
     end;
+    [5571328985] = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/foogledev/foogleclubfr/main/games/bloodlines.lua"))() 
+    end;
     [8304191830] = function()
-
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/foogledev/foogleclubfr/main/games/animeadventures.lua"))() 
     end;
 }
 if getgenv().Games[game.PlaceId] then
